7f0a0033391eaf6548f56bb2ee67fcda4f8a8313
M0H1K4N <nejer911@gmail.com>
2023-11-25 17:55:26 +0400

    add

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App runnint on port ${port}...`)
+})
+
+
