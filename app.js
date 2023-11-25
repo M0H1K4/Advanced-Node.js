caadde60897d0c12f7f2d6bc32745fac2267ddc3
M0H1K4N <nejer911@gmail.com>
2023-11-25 18:20:32 +0400

    First touch with postman

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// http მეთოდი
 app.get('/', (req, res) => {
-    
-})
+  res
+    .status(404)
+    .json({ message: 'Hello From the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.end('This is post method');
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App runnint on port ${port}...`)
-})
-
-
+  console.log(`App running on port ${port}...`);
+});
