55a79bc2d018720289a0bc214e04625e02a44a88
M0H1K4N <nejer911@gmail.com>
2023-11-26 14:41:01 +0400

    Creating new tour obj logic

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 
 // middleware || in the middle of req and res
-app.use(express.json())
+app.use(express.json());
 
 // http მეთოდი
 // app.get('/', (req, res) => {
@@ -34,10 +34,53 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.post('/api/v1/tours' , (req, res)  => {
-  console.log(req.body);
-  res.send('Done!')
-})
+// create new url and spasify only 1 tour
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  // convert string to nomber by |*1|
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if ( !tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'succes',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  // To add new tour in the Tours obj
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      // 201 means created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
