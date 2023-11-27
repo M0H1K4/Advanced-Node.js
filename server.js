9537490101539603f72e7740dbab56d58bf432db
M0H1K4N <nejer911@gmail.com>
2023-11-28 00:06:56 +0400

    Adding NODE_ENV

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const app = require('./app')
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: '/config.env' });
+
+// Get in Development Envierenment(variable)
+console.log(process.env.NODE_ENV);
 
 // START SERVER
 const port = 3000;
